be126c82bd87be6528f79cd99202000dc16685d1
Vijay <vijayganganapalli@gmail.com>
2019-06-26 22:11:44 +0530

    added form validation

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flearn/screens/design/tab_demo.dart';
+import 'package:flearn/screens/forms/FormValidation.dart';
 
 void main() => runApp(FLearnApp());
 
 class FLearnApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'FLearn App',
       theme: ThemeData(primarySwatch: Colors.blueGrey),
-      home: TabDemo(appBarTitle: 'Tab Demo'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Form Validation'),
+        ),
+        body: FormValidation(),
+      ),
     );
   }
 }
---
+++ lib/screens/forms/FormValidation.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class FormValidation extends StatefulWidget {
+  @override
+  _FormValidationState createState() => _FormValidationState();
+}
+
+class _FormValidationState extends State<FormValidation> {
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Enter some text';
+                }
+                return null;
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: RaisedButton(
+                child: Text('Submit'),
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    Scaffold.of(context).showSnackBar(
+                        SnackBar(content: Text('Processing data')));
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
